b7a06d3946420e7d76d1e0ccebf0fff6bbfb9399
zxt_0723 <zxt_0723@163.com>
2017-04-05 14:31:29 +0800

    添加跨域

---
+++ src/main/java/com/haibei/filter/BasicFilter.java
@@ -0,0 +1,49 @@
+package com.haibei.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+public class BasicFilter implements Filter {
+
+    public void destroy() {
+
+    }
+
+    public void doFilter(ServletRequest servletRequest,  ServletResponse servletResponse, FilterChain filterchain)
+			throws IOException, ServletException {
+
+     HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+ 
+
+//指定允许其他域名访问  
+
+ response.addHeader("Access-Control-Allow-Origin", "*");
+
+//2 响应类型  响应方法
+
+ response.addHeader("Access-Control-Allow-Methods", "POST, OPTIONS"); 
+
+   // 响应头设置 
+
+ response.addHeader("Access-Control-Allow-Headers", "POWERED-BY-FANTONG");
+
+   response.addHeader("Access-Control-Max-Age", "30");   
+ 
+ 
+ // 需要过滤的代码
+ filterchain.doFilter(servletRequest, servletResponse);
+}
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+}
